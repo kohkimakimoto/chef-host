19ebdae81c8d89c86eb11ba76aeab8ad53ba7231
Kohki Makimoto <kohki.makimoto@gmail.com>
2013-10-11 11:58:08 +0900

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,24 +1,22 @@
 #!/usr/bin/env bash
 
-REPODIR=/var/chef
+repodir=/var/chef
 
-REPOURL=""
+repourl=""
 if [ $# -ge 1  ]; then
-    REPOURL=$1
+    repourl=$1
     if [ $# -eq 2 ]; then
-        NODE=$2
+        node=$2
     fi
 else 
     echo "[Error] You must specify repository url"
     exit
 fi
 
-mkdir -p $REPODIR
-
 curl https://raw.github.com/kohkimakimoto/chef-host/master/chef-host > /usr/local/bin/chef-host
 chmod 755 /usr/local/bin/chef-host
 
-/usr/local/bin/chef-host
+git clone $repourl $repodir
 
 echo "Completed Installing. You can run chef-host instead of chef-solo."
 
